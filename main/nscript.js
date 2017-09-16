f5ff0a6a9d5314df082d38c714bf1e6d9f5c4d24
Hadithmv <hadithmv@users.noreply.github.com>
2017-09-15 21:39:19 -0800

    Update nscript.js

--- main/nscript.js
+++ main/nscript.js
@@ -44,7 +44,7 @@ $.fn.DataTable.ext.type.search.string = function ( data ) {
         '' :
         typeof data === 'string' ?
             data
-                .replace( /([^\u0621-\u063A\u0641-\u064A\u0660-\u0669a-zA-Z 0-9])/g, '')
+                .replace( /([^\u0621-\u063A\u0641-\u064A\u0660-\u0669 a-zA-Z 0-9 \u0780-\u07BF ])/g, '')
                 .replace(/(آ|إ|أ)/g, 'ا')
                 .replace(/(ة)/g, 'ه')
                 .replace(/(ئ|ؤ)/g, 'ء')
